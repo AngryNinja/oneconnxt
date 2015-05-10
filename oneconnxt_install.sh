7c7f30feda4a33b379e6d092fa3466ff1f357236
AngryNinja <AngryNinja@users.noreply.github.com>
2015-05-10 10:29:01 -0400

    Update oneconnxt_install.sh

--- oneconnxt_install.sh
+++ oneconnxt_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 os=$(uname -a | awk '{print $1}')
 if [ $os == 'Linux' ]; then
-    brew uninstall pkg-config yasm x264 x265 fdk-aac rtmpdump
     brew install pkg-config yasm x264 x265 fdk-aac rtmpdump
     export PKG_CONFIG_PATH=$HOME/.linuxbrew/lib/pkg-config
 elif [ $os == "Darwin" ]; then
@@ -12,41 +11,6 @@ rm -rf $HOME/Sources/temp
 mkdir $HOME/Sources/temp
 sudo chown $USER $HOME/Sources/temp
 cd $HOME/Sources/temp
-
-#git clone git://git.ffmpeg.org/rtmpdump
-#cd rtmpdump
-#sudo make CRYPTO=""  install
-#cd ..
-#wget http://downloads.sourceforge.net/opencore-amr/fdk-aac-0.1.2.tar.gz
-#tar xzvf fdk-aac-0.1.2.tar.gz
-#cd fdk-aac-0.1.2
-#./configure 
-#make
-#sudo make install 
-#cd ..
-
-#wget http://www.tortall.net/projects/yasm/releases/yasm-1.2.0.tar.gz
-#tar xvf yasm-1.2.0.tar.gz
-#cd yasm-1.2.0
-#./configure 
-#make
-#sudo make install
-
-#cd ..
-
-#git clone git://git.videolan.org/x264.git
-#cd x264
-#./configure  --enable-static --enable-shared --disable-lavf
-#make
-#sudo make install 
-#cd ..
-
-#git clone git://github.com/videolan/x265
-#cd x265/build
-#cmake ../source
-#make 
-#sudo make install
-#cd ../..
 
 git clone git://github.com/libav/libav
 sudo chown $USER $HOME/Sources/temp/libav
